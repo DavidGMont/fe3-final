77d0c8cd1c21d3ca5b9dcd60f4d616590bc5a7c4
DavidGMont <davidgarmo@outlook.com>
2023-09-10 22:48:08 -0500

    💳 Lógica preliminar de Card implementada y usada en Home

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Card = ({ name, username, id }) => {
     const addFav = () => {
         // Aqui iria la logica para agregar la Card en el localStorage
@@ -6,7 +8,9 @@ const Card = ({ name, username, id }) => {
     return (
         <div className="card">
             {/* En cada card deberán mostrar en name - username y el id */}
-
+            <p>{name}</p>
+            <p>{username}</p>
+            <p>{id}</p>
             {/* No debes olvidar que la Card a su vez servirá como Link hacia la pagina de detalle */}
 
             {/* Además, deberán integrar la lógica para guardar cada Card en el localStorage */}
@@ -15,6 +19,12 @@ const Card = ({ name, username, id }) => {
             </button>
         </div>
     );
+};
+
+Card.propTypes = {
+    name: PropTypes.string,
+    username: PropTypes.string,
+    id: PropTypes.number,
 };
 
 export default Card;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,10 +1,24 @@
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
+import { useGlobalStates } from '../Components/utils/global.context.jsx';
+import Card from '../Components/Card.jsx';
+
 const Home = () => {
+    const { data } = useGlobalStates();
     return (
         <main className="">
             <h1>Home</h1>
-            <div className="card-grid">{/* Aqui deberias renderizar las cards */}</div>
+            <div className="card-grid">
+                {/* Aqui deberias renderizar las cards */}
+                {data.map((dentist) => (
+                    <Card
+                        name={dentist.name}
+                        username={dentist.username}
+                        id={dentist.id}
+                        key={dentist.id}
+                    />
+                ))}
+            </div>
         </main>
     );
 };
