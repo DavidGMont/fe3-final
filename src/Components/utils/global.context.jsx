c791e790c57c1691ea02ec19a70d27d65dd61baa
DavidGMont <davidgarmo@outlook.com>
2023-09-10 22:46:47 -0500

    🌐 Lógica del Contexto Global implementada

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,11 +1,39 @@
-import { createContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-export const initialState = { theme: '', data: [] };
+export const initialState = { isDark: false, data: [] };
 
-export const ContextGlobal = createContext(undefined);
+export const GlobalStates = createContext(initialState);
 
-export const ContextProvider = ({ children }) => {
-    //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+const GlobalContext = ({ children }) => {
+    const [isDark, setIsDark] = useState(false);
+    const [data, setData] = useState([]);
 
-    return <ContextGlobal.Provider value={{}}>{children}</ContextGlobal.Provider>;
+    const URI = 'https://jsonplaceholder.typicode.com/users';
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios(URI);
+                console.log(response.data);
+                setData(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
+
+    return (
+        <GlobalStates.Provider value={{ isDark, data, setIsDark }}>
+            {children}
+        </GlobalStates.Provider>
+    );
 };
+
+GlobalContext.propTypes = {
+    children: PropTypes.any,
+};
+
+export default GlobalContext;
+
+export const useGlobalStates = () => useContext(GlobalStates);
