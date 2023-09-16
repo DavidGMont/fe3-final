511d70aa85b226b40878d7570212effcc749e44d
DavidGMont <davidgarmo@outlook.com>
2023-09-16 06:54:50 -0500

    🌟 Favoritos estilizados

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,16 +1,15 @@
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
 import { useGlobalStates } from '../Context/Context.jsx';
 import Card from '../Components/Card.jsx';
+import styles from '/src/styles/Favs.module.scss';
 
 const Favs = () => {
     const { state } = useGlobalStates();
     return (
         <>
-            <h1>Dentists Favs</h1>
-            <div className="card-grid">
-                {/* este componente debe consumir los destacados del localStorage */}
-                {/* Deberan renderizar una Card por cada uno de ellos */}
+            <h1>
+                <i className="fa-solid fa-star"></i> Favorite Dentists
+            </h1>
+            <div className={styles.home}>
                 {state.favorites.map((dentist) => (
                     <Card dentist={dentist} key={dentist.id} />
                 ))}
