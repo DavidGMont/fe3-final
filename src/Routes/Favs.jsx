43c5377f2d176dcf1ba5afc03f2315959c35dcca
DavidGMont <davidgarmo@outlook.com>
2023-09-16 08:54:56 -0500

    🎨 Corrección de problemas de espaciado

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,6 +1,7 @@
 import { useGlobalStates } from '../Context/Context.jsx';
 import Card from '../Components/Card.jsx';
 import styles from '/src/styles/Favs.module.scss';
+import Info from '../Components/Info.jsx';
 
 const Favs = () => {
     const { state } = useGlobalStates();
@@ -10,9 +11,17 @@ const Favs = () => {
                 <i className="fa-solid fa-star"></i> Favorite Dentists
             </h1>
             <div className={styles.home}>
-                {state.favorites.map((dentist) => (
-                    <Card dentist={dentist} key={dentist.id} />
-                ))}
+                {state.favorites.length > 0 ? (
+                    state.favorites.map((dentist) => <Card dentist={dentist} key={dentist.id} />)
+                ) : (
+                    <Info
+                        color={'#9b5151'}
+                        feedback={
+                            "You don't have any favorite dentist yet, add at least one to preview it here!"
+                        }
+                        icon={'fa-solid fa-circle-xmark'}
+                    />
+                )}
             </div>
         </>
     );
